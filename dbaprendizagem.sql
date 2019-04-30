230c65f964f85c041320c23288197afe46c2a25a
GuilhermeReis9876 <guilhermereis9876@gmail.com>
2019-04-30 13:30:07 -0300

    banco de dados atualizdo 1.0

--- dbaprendizagem.sql
+++ dbaprendizagem.sql
@@ -1,5 +1,5 @@
-create schema biblioteca;
-use biblioteca;
+create schema dbaprendizagem;
+use dbaprendizagem;
 
 create table Func (
     idFunc smallint primary key,
@@ -13,11 +13,4 @@ create table Alunos (
     idAluno smallint primary key,
     nomeAluno varchar(60) not null,
     emailAluno varchar(60) not null,
-);
-
-create table Livros (
-    idLivro smallint primary key,
-    nomeLivro varchar(60) not null,
-    autorLivro varchar(60) not null,
-    categoriaLivro varchar(20) not null,
 );
