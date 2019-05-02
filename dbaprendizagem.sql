8ae3668c75068e8e9898a21a6a86da231b594f78
Thiago Mattos <thiagobuenosairesmattos@gmail.com>
2019-05-02 15:13:15 -0300

    Banco de dados

--- dbaprendizagem.sql
+++ dbaprendizagem.sql
@@ -1,16 +1,40 @@
-create schema dbaprendizagem;
-use dbaprendizagem;
+CREATE schema dbaprendizagem;
+USE dbaprendizagem;
 
-create table Func (
-    idFunc smallint primary key,
-    nomeFunc varchar(60) not null,
-    emailFunc varchar(60) not null,
-    senhaFunc varchar(20) not null,
-    cpfFunc varchar(14) not null
+CREATE TABLE Func (
+    idFunc SMALLINT PRIMARY KEY,
+    nomeFunc VARCHAR(60) NOT NULL,
+    emailFunc VARCHAR(60) NOT NULL,
+    senhaFunc VARCHAR(20) NOT NULL,
+    cpfFunc VARCHAR(14) NOT NULL
 );
 
-create table Alunos (
-    idAluno smallint primary key,
-    nomeAluno varchar(60) not null,
-    emailAluno varchar(60) not null,
+CREATE TABLE Alunos(
+    Ra VARCHAR(10),
+    Nome VARCHAR(100),
+    DataNasc VARCHAR(10),
+    Idade INT,
+    Sexo CHAR(1),
+    GrauInstrucao VARCHAR(30),
+    Rua VARCHAR(50),
+    Numero INT ,
+    Complemento VARCHAR(50),
+    Bairro VARCHAR(30),
+    Estado CHAR(2),
+    Cidade VARCHAR(30),
+    Cep VARCHAR(8),
+    Telefone1 VARCHAR(15),
+    Telefone2 VARCHAR(15),
+    Identidade VARCHAR(30),
+    Cpf VARCHAR(11),
+    Email VARCHAR(50),
+    CarteiraTrabalho VARCHAR(20),
+    NomePai VARCHAR(100),
+    TelefonePai VARCHAR(100),
+    NomeMae VARCHAR(100),
+    TelefoneMae VARCHAR(100),
+    NomeCurso VARCHAR(50),
+    CodTurma VARCHAR(20),
+    Status VARCHAR(30),
+    PRIMARY KEY(Ra)
 );
