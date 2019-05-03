13d5eda48312f3122506d9bb2732db6baa178ba3
Thiago Mattos <thiagobuenosairesmattos@gmail.com>
2019-05-03 16:36:19 -0300

    Model BuscaAvanÃcada

--- models/buscarAlunos.php
+++ models/buscarAlunos.php
@@ -4,6 +4,7 @@
     $nome = ($_POST['nome']);
     require_once('../class/ClassAlunos.php');
     $objAlunos = new ClassAlunos();
+    $objAlunos->setNome($nome);
     $queryResp = $objAlunos->BuscaAlunos($nome);
 
     echo json_encode($queryResp);
