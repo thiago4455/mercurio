316e975f8620dc2d43a4f6b28fa76199f6dc9c70
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2019-05-02 23:06:34 -0300

    SignOut

--- signout.php
+++ signout.php
@@ -14,11 +14,8 @@
     <!-- Fontes -->
     <link rel="stylesheet" href="fonts/fontes.css">
 
-    <!-- Loading.css -->
-    <link rel="stylesheet" href="css/loading.css">
-
-    <!-- Login.css -->
-    <link rel="stylesheet" href="css/login.css">
+    <!-- Signout.css -->
+    <link rel="stylesheet" href="css/signout.css">
 
     <!-- Navbar.css -->
     <link rel="stylesheet" href="css/navbar.css">
@@ -28,22 +25,13 @@
 
     <?php
         session_start();
-        if(isset($_SESSION['idLog'])) {
-            header('Location: home.php');
+        if(!isset($_SESSION['idLog'])) {
+            header('Location: index.php');
+        }
+        else {
+            session_destroy();
         }
     ?>
-
-
-    <!-- Loading Page -->
-    <div id="page-load">
-		<h1>Senai</h1>
-		<div class="lds-ellipsis">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
 
 
     <!-- Navbars -->
@@ -56,24 +44,8 @@
     <!-- --------------------------- -->
 
     <div id="main">
-        <div id="section-login">
-            <h1 id="form-title">Login</h1>
-            <input name="ipt_email" id="ipt_email" type="email" class="input-login" placeholder="Digite seu email"
-                require>
-            <input name="ipt_senha" id="ipt_senha" type="password" class="input-login"
-                placeholder="Digite sua senha" require>
-            <div id="alert-error" class="alert alert-danger alert-dismissible fade show" role="alert">
-                <button type="button" class="close" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <p id="error-msg"></p>
-            </div>
-            <button name="btn_login" class="btn" id="btn-login">Logar</button>
-        </div>
+        <h1>Deslogando...</h1>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
     <script
         src="https://code.jquery.com/jquery-3.4.0.min.js"
@@ -83,7 +55,14 @@
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 
-    <script src="js/login.js"></script>
+    <script>
+        $(window).on('load', function(){
+            setTimeout(() => {
+                window.location.href = 'index.php';
+            }, 1500);
+        })
+    </script>
+
 
 </body>
 
