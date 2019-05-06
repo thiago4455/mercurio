4c622bc3d6cf0b5b66d7e398d6db2e2a21dca68f
Thiago Mattos <thiagobuenosairesmattos@gmail.com>
2019-05-06 16:13:02 -0300

    Importaçao Excel

---
+++ models/importarExcel.php
@@ -0,0 +1,22 @@
+<?php
+
+    set_time_limit(0);
+
+    # Informa qual o conjunto de caracteres será usado.
+    header('Content-Type: text/html; charset=utf-8');
+
+    $csvTable = $_POST['csv'];
+
+    $filename = "../csv/".time().".csv";
+    $myfile = fopen($filename, "w");
+    fwrite($myfile, $csvTable);
+    fclose($myfile);
+
+    if($csvTable!=''){
+        $link = mysqli_connect("localhost", "root", "root", "dbaprendizagem");
+
+        $query = "LOAD DATA LOCAL INFILE '".str_replace("\\",'/',__DIR__)."/".$filename."' REPLACE INTO TABLE Alunos FIELDS TERMINATED BY ',' IGNORE 1 LINES (Ra,Nome,DataNasc,Idade,Sexo,GrauInstrucao,Rua,Numero,Complemento,Bairro,Estado,Cidade,Cep,Telefone1,Identidade,Cpf,Email,CarteiraTrabalho,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,NomePai,TelefonePai,@dummy,@dummy,@dummy,@dummy,NomeMae,TelefoneMae,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,NomeCurso,@dummy,CodTurma,@dummy,Status,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,Telefone2,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy);";
+        echo $query;
+        $objConexao->executarComandoSQL($query);   
+    }
+?>
