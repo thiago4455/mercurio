8f45ba4e514d824d70f3ec77fe3f3ca284566fa9
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2019-05-13 23:42:12 -0300

    Melhorias

--- controllers/importarExcel.php
+++ controllers/importarExcel.php
@@ -15,6 +15,8 @@
         require_once('../models/ConexaoClass.php');
         $objConexao = new ConexaoClass("localhost", "root", "root", "dbmercurio");
 
+        mysqli_options($objConexao, MYSQLI_OPT_LOCAL_INFILE, true);
+
         $query = "LOAD DATA LOCAL INFILE '".str_replace("\\",'/',__DIR__)."/".$filename."' REPLACE INTO TABLE Alunos FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES (Ra,Nome,DataNasc,Idade,Sexo,GrauInstrucao,Rua,Numero,Complemento,Bairro,Estado,Cidade,Cep,Telefone1,Identidade,Cpf,Email,CarteiraTrabalho,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,NomePai,TelefonePai,@dummy,@dummy,@dummy,@dummy,NomeMae,TelefoneMae,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,NomeCurso,@dummy,CodTurma,@dummy,Status,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,Telefone2,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy) SET Semestre = '".date("m.y") ."';";
         echo $query;
         $objConexao->executarComandoSQL($query);   
