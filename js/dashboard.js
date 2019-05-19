8f5523c6a060dffcf64c76bdba5bc5fa5c66969f
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2019-05-19 20:47:45 -0300

    dash 1.1

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,15 +9,12 @@ $(document).ready(function () {
         type: 'GET',
         dataType: 'json',
         success: function (msg) {
-            console.log(msg)
-            
-
             $('#info-ciclos').text(msg[0].length)
             $('#info-alunosCadastrados').text(msg[1].length)
             $('#info-funcCadastrados').text(msg[2].length)
             $('#info-empCadastrados').text(msg[3].length)
             $('#info-alunosEncaminhados').text(msg[4].length)
-            $('#info-relatorios').text((msg[5] - 2))
+            $('#info-relatorios').text((msg[5] - 3))
 
         },
         error: function (err) {
