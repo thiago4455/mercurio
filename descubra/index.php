69ca1dde6f924457367174df3aef8fee387e6f47
GuilhermeReis9876 <guilhermereis9876@gmail.com>
2019-05-22 15:10:54 -0300

    s

--- descubra/index.php
+++ descubra/index.php
@@ -98,14 +98,9 @@
           <div class="col-lg-7 col-md-6">
             <div class="about-content">
               <h2>Sobre</h2>
-              <h3>Odio sed id eos et laboriosam consequatur eos earum soluta.</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-              <p>Aut dolor id. Sint aliquam consequatur ex ex labore. Et quis qui dolor nulla dolores neque. Aspernatur consectetur omnis numquam quaerat. Sed fugiat nisi. Officiis veniam molestiae. Et vel ut quidem alias veritatis repudiandae ut fugit. Est ut eligendi aspernatur nulla voluptates veniam iusto vel quisquam. Fugit ut maxime incidunt accusantium totam repellendus eum error. Et repudiandae eum iste qui et ut ab alias.</p>
-              <ul>
-                <li><i class="ion-android-checkmark-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                <li><i class="ion-android-checkmark-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                <li><i class="ion-android-checkmark-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-              </ul>
+              <h3>O Sistema Mercurio é a melhor solução para o gestão de alunos.</h3>
+              <p>Cansado de planilhas gigantes e complexas, e de passar horas criando relatórios e mais relatórios? Nós sabemos o quanto isso é chato, e queremos facilitar a sua vida.</p>
+              <p>O Sistema Mercurio é uma solução Web que te permite ficar longe de qualquer complexidade. Nós entendemos sobre os processos ligados a gestão de alunos e queremos trazer o melhor para você. Com uma interface moderna, intuitiva e simples trazemos vários recursos que vão ajudar muito seu dia a dia.</p>
             </div>
           </div>
         </div>
@@ -177,63 +172,6 @@
       </div>
     </section><!-- #services -->
 
-    <!--==========================
-      Why Us Section
-    ============================-->
-    <section id="why-us" class="wow fadeIn">
-      <div class="container-fluid">
-        
-        <header class="section-header">
-          <h3>Why choose us?</h3>
-          <p>Laudem latine persequeris id sed, ex fabulas delectus quo. No vel partiendo abhorreant vituperatoribus.</p>
-        </header>
-
-        <div class="row">
-
-          <div class="col-lg-6">
-            <div class="why-us-img">
-              <img src="descubra/img/why-us.jpg" alt="" class="img-fluid">
-            </div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="why-us-content">
-              <p>Molestiae omnis numquam corrupti omnis itaque. Voluptatum quidem impedit. Odio dolorum exercitationem est error omnis repudiandae ad dolorum sit.</p>
-              <p>
-                Explicabo repellendus quia labore. Non optio quo ea ut ratione et quaerat. Porro facilis deleniti porro consequatur
-                et temporibus. Labore est odio.
-
-                Odio omnis saepe qui. Veniam eaque ipsum. Ea quia voluptatum quis explicabo sed nihil repellat..
-              </p>
-
-              <div class="features wow bounceInUp clearfix">
-                <i class="fa fa-diamond" style="color: #f058dc;"></i>
-                <h4>Corporis dolorem</h4>
-                <p>Commodi quia voluptatum. Est cupiditate voluptas quaerat officiis ex alias dignissimos et ipsum. Soluta at enim modi ut incidunt dolor et.</p>
-              </div>
-
-              <div class="features wow bounceInUp clearfix">
-                <i class="fa fa-object-group" style="color: #ffb774;"></i>
-                <h4>Eum ut aspernatur</h4>
-                <p>Molestias eius rerum iusto voluptas et ab cupiditate aut enim. Assumenda animi occaecati. Quo dolore fuga quasi autem aliquid ipsum odit. Perferendis doloremque iure nulla aut.</p>
-              </div>
-              
-              <div class="features wow bounceInUp clearfix">
-                <i class="fa fa-language" style="color: #589af1;"></i>
-                <h4>Voluptates dolores</h4>
-                <p>Voluptates nihil et quis omnis et eaque omnis sint aut. Ducimus dolorum aspernatur. Totam dolores ut enim ullam voluptas distinctio aut.</p>
-              </div>
-              
-            </div>
-
-          </div>
-
-        </div>
-
-      </div>
-
-    </section>
-
     <section id="call-to-action" class="wow fadeInUp">
       <div class="container">
         <div class="row counters">
@@ -268,36 +206,21 @@
     <section id="features">
       <div class="container">
 
-        <div class="row feature-item">
-          <div class="col-lg-6 wow fadeInUp">
-            <img src="descubra/img/features-1.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 wow fadeInUp pt-5 pt-lg-0">
-            <h4>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h4>
-            <p>
-              Ipsum in aspernatur ut possimus sint. Quia omnis est occaecati possimus ea. Quas molestiae perspiciatis occaecati qui rerum. Deleniti quod porro sed quisquam saepe. Numquam mollitia recusandae non ad at et a.
-            </p>
-            <p>
-              Ad vitae recusandae odit possimus. Quaerat cum ipsum corrupti. Odit qui asperiores ea corporis deserunt veritatis quidem expedita perferendis. Qui rerum eligendi ex doloribus quia sit. Porro rerum eum eum.
-            </p>
-          </div>
-        </div>
-
         <div class="row feature-item mt-5 pt-5">
           <div class="col-lg-6 wow fadeInUp order-1 order-lg-2">
             <img src="descubra/img/features-2.svg" class="img-fluid" alt="">
           </div>
 
           <div class="col-lg-6 wow fadeInUp pt-4 pt-lg-0 order-2 order-lg-1">
-            <h4>Neque saepe temporibus repellat ea ipsum et. Id vel et quia tempora facere reprehenderit.</h4>
+            <h4>Onde estiver e quando quiser, o Mercurio pode estar até em seu celular. </h4>
             <p>
-             Delectus alias ut incidunt delectus nam placeat in consequatur. Sed cupiditate quia ea quis. Voluptas nemo qui aut distinctio. Cumque fugit earum est quam officiis numquam. Ducimus corporis autem at blanditiis beatae incidunt sunt. 
+              Precisa se locomover mas não quer perder tempo? Não tem problema, o Mercurio vai com você independente de onde você estiver. 
             </p>
             <p>
-              Voluptas saepe natus quidem blanditiis. Non sunt impedit voluptas mollitia beatae. Qui esse molestias. Laudantium libero nisi vitae debitis. Dolorem cupiditate est perferendis iusto.
+              O sistema é totalmente responsivo, podendo ser executado em computadores e smartphone através do navegador.
             </p>
             <p>
-              Eum quia in. Magni quas ipsum a. Quis ex voluptatem inventore sint quia modi. Numquam est aut fuga mollitia exercitationem nam accusantium provident quia.
+              Com o Mercurio você pode ter o seu trabalho na palma da sua mão, facilitando a assistência aos alunos e funcionários.
             </p>
           </div>
           
@@ -313,7 +236,7 @@
       <div class="container">
         <div class="section-header">
           <h3>Time</h3>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+          <p>Um grande projeto depende de um grande time. Feito com carinho por esses grandes profissionais.</p>
         </div>
 
         <div class="row">
@@ -328,7 +251,7 @@
                   <div class="social">
                   <a href="https://github.com/thiago4455" target="_blank"><i class="fa fa-github"></i></a>
                     <a href="https://www.instagram.com/thiagobamattos/?hl=pt-br" target="_blank"><i class="fa fa-instagram"></i></a>
-                    <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    <a href="https://www.linkedin.com/in/thiago-mattos-1a080aab" target="_blank"><i class="fa fa-linkedin"></i></a>
                   </div>
                 </div>
               </div>
@@ -428,45 +351,26 @@
                 <div class="col-sm-6">
 
                   <div class="footer-info">
-                    <h3>Rapid</h3>
+                    <h3>Mercurio</h3>
                     <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-                  </div>
-
-                  <div class="footer-newsletter">
-                    <h4>Our Newsletter</h4>
-                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem.</p>
-                    <form action="" method="post">
-                      <input type="email" name="email"><input type="submit"  value="Subscribe">
-                    </form>
                   </div>
 
                 </div>
 
                 <div class="col-sm-6">
                   <div class="footer-links">
-                    <h4>Useful Links</h4>
+                    <h4>Links Úteis</h4>
                     <ul>
                       <li><a href="#">Home</a></li>
-                      <li><a href="#">About us</a></li>
-                      <li><a href="#">Services</a></li>
-                      <li><a href="#">Terms of service</a></li>
-                      <li><a href="#">Privacy policy</a></li>
+                      <li><a href="#">Sobre Nós</a></li>
+                      <li><a href="#">Serviços</a></li>
+                      <li><a href="#">Termos de Serviço</a></li>
+                      <li><a href="#">Política de Privacidade</a></li>
                     </ul>
                   </div>
 
-                  <div class="footer-links">
-                    <h4>Contact Us</h4>
-                    <p>
-                      A108 Adam Street <br>
-                      New York, NY 535022<br>
-                      United States <br>
-                      <strong>Phone:</strong> +1 5589 55488 55<br>
-                      <strong>Email:</strong> info@example.com<br>
-                    </p>
-                  </div>
-
                   <div class="social-links">
-                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="#" class="github"><i class="fa fa-github "></i></a>
                     <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
                     <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
                     <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
@@ -482,30 +386,30 @@
 
             <div class="form">
               
-              <h4>Send us a message</h4>
-              <p>Eos ipsa est voluptates. Nostrum nam libero ipsa vero. Debitis quasi sit eaque numquam similique commodi harum aut temporibus.</p>
+              <h4>Mande-nos uma mensagem</h4>
+              <p>A sua opinião é muito importante para nós. Além disse, se tiver alguma dúvida, sugestão ou reclamação, envie-nos uma mensagem abaixo.</p>
               <form action="" method="post" role="form" class="contactForm">
                 <div class="form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Seu Nome" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                   <div class="validation"></div>
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Seu Email" data-rule="email" data-msg="Please enter a valid email" />
                   <div class="validation"></div>
                 </div>
                 <div class="form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Assunto" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
                   <div class="validation"></div>
                 </div>
                 <div class="form-group">
-                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Digite sua mensagem..."></textarea>
                   <div class="validation"></div>
                 </div>
 
-                <div id="sendmessage">Your message has been sent. Thank you!</div>
+                <div id="sendmessage">Sua mensagem foi enviada. Obrigado!</div>
                 <div id="errormessage"></div>
 
-                <div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
+                <div class="text-right"><button type="submit" title="Send Message">Enviar Mensagem</button></div>
               </form>
             </div>
 
@@ -520,7 +424,7 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong>Rapid</strong>. All Rights Reserved
+        &copy; Copyright <strong>Mercurio</strong>. Todos os direitos reservados
       </div>
       <div class="credits">
         <!--
