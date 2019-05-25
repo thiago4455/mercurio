418a76e5d412783d1c0d37c493b4acb368e07b6a
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2019-05-24 21:57:31 -0300

    Correções no Banco de Dados

--- dbmercurio.sql
+++ dbmercurio.sql
@@ -99,9 +99,9 @@ CREATE TABLE Historico(
     FOREIGN KEY (empresaCod)
     REFERENCES Empresas(codEmpresa),
     funcionarioId INT,
-    FOREIGN KEY (funcionarioCod)
+    FOREIGN KEY (funcionarioId)
     REFERENCES Funcionarios(idFunc),
-    descricao VARCHAR(100)
+    descricao TEXT
 )CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 INSERT INTO `Funcionarios` (`idFunc`, `nomeFunc`, `emailFunc`, `senhaFunc`, `cpfFunc`, `telefoneFunc`, `cepFunc`, `ruaFunc`, `numeroFunc`, `bairroFunc`, `cidadeFunc`, `estadoFunc`, `tipoFunc`) VALUES ('1', 'Admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', '000.000.000-00', '(00) 00000-0000', '00000-000', 'Rua X', '0', 'Bairro X', 'Cidade X', '00', 'admin');
